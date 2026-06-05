f84c4faeef4469e85a98db3a83cc7004aff04d4c
aphynt <ahmadfadillah502@gmail.com>
2026-06-05 14:43:10 +0800

    -

--- app/Http/Controllers/Produksi/KKHController.php
+++ app/Http/Controllers/Produksi/KKHController.php
@@ -76,7 +76,17 @@ class KKHController extends Controller
             $currentUserRole = 'SUPERVISOR';
         }
 
-        $kkh = DB::connection('kkh')->table('db_payroll.dbo.web_kkh as kkh')
+        $latestIds = DB::connection('kkh')
+            ->table('db_payroll.dbo.web_kkh')
+            ->where('tgl', $tanggalKKH)
+            ->groupBy('nik')
+            ->selectRaw('MAX(id) as id');
+
+        $kkh = DB::connection('kkh')
+            ->table('db_payroll.dbo.web_kkh as kkh')
+            ->joinSub($latestIds, 'latest', function ($join) {
+                $join->on('kkh.id', '=', 'latest.id');
+            })
             ->leftJoin('db_payroll.dbo.tbl_data_hr as hr', 'kkh.nik', '=', 'hr.nik')
             ->leftJoin('db_payroll.dbo.tbl_data_hr as hr2', 'kkh.nik_pengawas', '=', 'hr2.nik')
             ->leftJoin('db_payroll.dbo.tm_departemen as dp', 'hr.ID_Departemen', '=', 'dp.ID_Departemen')
@@ -380,7 +390,17 @@ class KKHController extends Controller
             $currentUserRole = 'SUPERVISOR';
         }
 
-        $kkh = DB::connection('kkh')->table('db_payroll.dbo.web_kkh as kkh')
+        $latestIds = DB::connection('kkh')
+            ->table('db_payroll.dbo.web_kkh')
+            ->whereBetween('tgl', [$startTimeFormatted, $endTimeFormatted])
+            ->groupBy('nik')
+            ->selectRaw('MAX(id) as id');
+
+        $kkh = DB::connection('kkh')
+            ->table('db_payroll.dbo.web_kkh as kkh')
+            ->joinSub($latestIds, 'latest', function ($join) {
+                $join->on('kkh.id', '=', 'latest.id');
+            })
             ->leftJoin('db_payroll.dbo.tbl_data_hr as hr', 'kkh.nik', '=', 'hr.nik')
             ->leftJoin('db_payroll.dbo.tbl_data_hr as hr2', 'kkh.nik_pengawas', '=', 'hr2.nik')
             ->leftJoin('db_payroll.dbo.tm_departemen as dp', 'hr.Id_Departemen', '=', 'dp.ID_Departemen')
