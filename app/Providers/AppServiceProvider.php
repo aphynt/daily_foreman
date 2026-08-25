cb448eed7180b2b86bce4570f2a0e287d8e9b76a
aphynt <ahmadfadillah502@gmail.com>
2026-08-25 08:27:22 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        //URL::forceScheme('https');
+        URL::forceScheme('https');
 
         require_once app_path('Helpers/RoleHelper.php');
         Event::listen(\Illuminate\Console\Events\CommandStarting::class, function ($event) {
